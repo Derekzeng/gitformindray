6205cb6fdbc075d956c4d97b6c5518eec4d9a797
50270164 <zengbin@mindray.com>
2024-08-11 11:06:59 +0800

    Revert "修改业务，编译，然后提交发布"
    This reverts commit 06d6dc9e

--- src/main/java/com/mindray/cis/hospital/SampleAdtQueryFromOracle.java
+++ src/main/java/com/mindray/cis/hospital/SampleAdtQueryFromOracle.java
@@ -36,8 +36,6 @@ public class SampleAdtQueryFromOracle implements IClient, IAdtQuery {
             }catch (Exception e){
                 logger.info("[error:]{},返回空的集合.",e.getMessage());
             }
-            logger.info("");
-            logger.info("master 添加的日志，合并的时候会有冲突.");
 
         }
         return adtResponses;
